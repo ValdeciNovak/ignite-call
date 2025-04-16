7422917bb9d60cc4ed3b1bb31344483ed8c196f4
ValdeciNovak <valdecirnovak@gmail.com>
2025-04-15 23:56:08 -0300

    Adjusted for vercel

--- next.config.js
+++ next.config.js
@@ -1,21 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  pageExtensions: [
-    'page.tsx',
-    'api.ts',
-    'api.tsx'
-  ],
+  // swcMinify: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
 
-  webpack: (config) => {
+  webpack: config => {
     config.watchOptions = {
       poll: 1000, // Verifica a cada segundo
       aggregateTimeout: 300,
-      ignored: /node_modules/
+      ignored: /node_modules/,
     }
     return config
-  }
+  },
 }
 
 module.exports = nextConfig
